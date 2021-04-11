9459f20cc8d3cbddbe02b5d7d5fa5354d14c1edb
Alex <ale.krotov@gmail.com>
2021-04-11 11:44:26 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+for (let i = 1;i<=10; i++){
+  console.log('${i}*${i}=${i*i}');
+}
+let firstName = prompt('Insert firstname')
+let lastName = prompt('Insert lastname')
+let name = prompt('Insert name')
+alert(firstName+' ' + lastName)
